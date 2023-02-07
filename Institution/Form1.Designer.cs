27e2128885899926feae7ca91cafee11233d21b6
Andrei <ikar2008@hotmail.com>
2023-02-07 14:30:16 +0300

    add name Form1

--- Institution/Form1.Designer.cs
+++ Institution/Form1.Designer.cs
@@ -202,6 +202,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.Text = "Institution";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
